3011d75f4da396878d061f02059d2502339e62b5
git-mih <git.mih@gmail.com>
2021-09-17 15:02:42 -0300

    add index.html and main.js files

---
+++ src/main.js
@@ -0,0 +1,34 @@
+
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        this.tick();
+        this.intervalID = setInterval(this.tick, 1000);
+    //                     setInterval -> int (interval ID)
+    }
+
+    tick = () => {
+        console.log('tick..')
+    }
+
+    pause = () => {
+        console.log(`clear interval id: ${this.intervalID}`)
+        clearInterval(this.intervalID);
+    }
+
+}
+
+const durationInput = document.querySelector('#duration');
+const startBtn = document.querySelector('#play-button');
+const pauseBtn = document.querySelector('#pause-button');
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
+

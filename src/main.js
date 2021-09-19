48963c6938d2d166028122286bf023ba83d6870f
git-mih <git.mih@gmail.com>
2021-09-18 21:03:37 -0300

    add svg animation

--- src/main.js
+++ src/main.js
@@ -1,44 +1,23 @@
-
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    start = () => {
-        this.tick();
-        this.intervalID = setInterval(this.tick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.intervalID);
-    }
-
-
-    tick = () => {
-        if (this.timeRemaining <= 0) {
-            this.pause();
-        } else {
-            this.timeRemaining = this.timeRemaining - 1;
-        }
-    }
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-
-}
-
 const durationInput = document.querySelector('#duration');
 const startBtn = document.querySelector('#play-button');
 const pauseBtn = document.querySelector('#pause-button');
 
-const timer = new Timer(durationInput, startBtn, pauseBtn);
+const circle = document.querySelector('circle');
+const circleRadius = circle.getAttribute('r')
+const perimeter = circleRadius * 2 * Math.PI; // ~1193
+circle.setAttribute('stroke-dasharray', perimeter);
 
+let currentOffset = 0;
+
+const timer = new Timer(durationInput, startBtn, pauseBtn, {
+    onStart() {
+        console.log('Time started')
+    },
+    onTick(time) {
+        circle.setAttribute('stroke-dashoffset', currentOffset);
+        currentOffset -= 1;
+    },
+    onComplete() {
+        console.log('Time is completed.')
+    }
+});
